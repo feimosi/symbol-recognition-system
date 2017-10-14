741b0262a6593fea1acce368b3f2e41d646f0ae9
AUGUSTYNUX303UB\augus <augustyn.mikolaj@gmail.com>
2017-10-14 08:32:57 +0200

    Ts check on nerual

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-const synaptic = require('synaptic');
+import { Neuron, Layer } from "synaptic";
 
 var x : number = 5;
 console.log('Hello TypeScript');
+
+var neuron : Neuron  = new Neuron();
+const layer: Layer = new Layer(3);
